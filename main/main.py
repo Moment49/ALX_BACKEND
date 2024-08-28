63f12e2e6be5a02c68d3944a53c595a4ee5e98ca
Moment49 <ibenachoelvis49@gmail.com>
2024-08-29 00:35:26 +0100

    updated

--- main/main.py
+++ main/main.py
@@ -4,4 +4,4 @@
 # In your terminal, make sure you’re in your repository’s directory.
 # Run the git log command: git log
 # You’ll see a list of commits, each with an author, date, and message. For a more condensed view, try git log --oneline.
-print("Hellow world! Welcome to Backend Engineering!!!!")
+print("Hellow world! Welcome to Backend Engineering!!1!")
